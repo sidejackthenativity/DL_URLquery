2b4f14c1e71e45539424e5b64ec7f7a5db338a07
sidejackthenativity <sidejackthenativity@gmail.com>
2021-04-17 21:58:39 -0400

    added: 1. the ability to add the results column based on user input 2. stores array as a csv file

--- convert_training_samples.py
+++ convert_training_samples.py
@@ -2,25 +2,43 @@
 
 from keras.preprocessing.sequence import pad_sequences
 import sys
+import numpy as np
+from numpy import savetxt
 
-#grab data from file
-file_location = sys.argv[1]
-list = []
+def do_things(file_location, column_value, file_name):
+    #grab data from file
+    list = []
 
-try:
-    file = open(file_location, 'r')
-    Lines = file.readlines()
+    try:
+        file = open(file_location, 'r')
+        Lines = file.readlines()
 
-except IOError:
-    print('There was an error opening the file!')
+    except IOError:
+        print('There was an error opening the file!')
 
-    # Strips the newline character
-for line in Lines:
-    list.append([ord(c) for c in line][:-2]) #remove newline and CR
-
-print(list)
+        # Strips the newline character
+    for line in Lines:
+        list.append([ord(c) for c in line][:-2]) #remove newline and CR
 
 
-# pad sequence
-padded = pad_sequences(list,maxlen=3063,padding='post')
-print(padded)
+    # pad sequence
+    padded = pad_sequences(list,maxlen=3063,padding='post')
+    #padded: 3 x 3063
+    N = padded.shape[0]
+    #now add the value 0 or 1 as declared in argument 2
+    if (column_value=='0'):
+        new_column = np.zeros((N,1)) #new_column: 3 x 1
+    elif (column_value=='1'):
+        new_column = np.ones((N,1)) #new_column: 3 x 1
+    #print(padded.shape)
+    #print(new_column.shape)
+    an_array = np.append(padded, new_column, axis=1)
+    savetxt(file_name, an_array, delimiter=',')
+
+
+
+#__main__
+if (len(sys.argv)==4):
+    do_things(sys.argv[1],sys.argv[2],sys.argv[3])
+else:
+    print("Proper format is: python3 convert_training_samples.py <data_source> <value as a string in last column> <destination file>")
